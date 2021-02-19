d04b3837cda2ef821ae40e0d0d7147a242266dc0
Tasfik Rahman <tasfikrahman001@gmail.com>
2021-02-20 02:07:22 +0600

    Create cpp17.cpp for problem 1001 (#9)
    * Create cpp17.cpp
    
    * spaces added
    
    * qualified variable names used
    
    * for loop used, in place of while loop
    
    * newline added at the end

---
+++ 1001/cpp17.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Returns  the number of problems stored in each computer.
+ * @param n denotes the total number of problems.
+ */
+vector<int> problemsStored(int n) {
+    // Implement this method
+
+}
+
+/**
+ * Takes care of the problem input and output.
+ */
+int main() {
+    int cases;
+    scanf("%d", &cases);
+    for (int caseno = 1; caseno <= cases; ++caseno) {
+        int n;
+        scanf("%d", &n);
+        
+        vector<int> ans = problemsStored(n);
+        printf("%d %d\n", ans[0], ans[1]);
+    }
+    return 0;
+}
+
